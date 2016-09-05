b9faeaf88a13e04f6cd4ad00c37a6a210e049fc6
kangcy <kangcy@axon.com.cn>
2016-09-05 18:20:47 +0800

    图标

--- EGT_OTA/Controllers/App/PayController.cs
+++ EGT_OTA/Controllers/App/PayController.cs
@@ -156,44 +156,54 @@ namespace EGT_OTA.Controllers
         [AllowAnyone]
         public ActionResult All()
         {
-            var pager = new Pager();
-            var query = new SubSonic.Query.Select(Repository.GetProvider()).From<Pay>().Where<Pay>(x => x.Status == Enum_Status.Approved);
-            var FromUserID = ZNRequest.GetInt("FromUserID");
-            var ToUserID = ZNRequest.GetInt("ToUserID");
-            if (FromUserID > 0)
+            var callback = ZNRequest.GetString("jsoncallback");
+            try
             {
-                query = query.And("FromUserID").IsEqualTo(FromUserID);
-            }
-            if (ToUserID > 0)
-            {
-                query = query.And("ToUserID").IsEqualTo(ToUserID);
-            }
-            var recordCount = query.GetRecordCount();
-            var totalPage = recordCount % pager.Size == 0 ? recordCount / pager.Size : recordCount / pager.Size + 1;
-            var list = query.Paged(pager.Index, pager.Size).OrderDesc("ID").ExecuteTypedList<Pay>();
+                var pager = new Pager();
+                var query = new SubSonic.Query.Select(Repository.GetProvider()).From<Pay>().Where<Pay>(x => x.Status == Enum_Status.Approved);
+                var FromUserID = ZNRequest.GetInt("FromUserID");
+                var ToUserID = ZNRequest.GetInt("ToUserID");
+                if (FromUserID > 0)
+                {
+                    query = query.And("FromUserID").IsEqualTo(FromUserID);
+                }
+                if (ToUserID > 0)
+                {
+                    query = query.And("ToUserID").IsEqualTo(ToUserID);
+                }
+                var recordCount = query.GetRecordCount();
+                var totalPage = recordCount % pager.Size == 0 ? recordCount / pager.Size : recordCount / pager.Size + 1;
+                var list = query.Paged(pager.Index, pager.Size).OrderDesc("ID").ExecuteTypedList<Pay>();
 
-            var fromarray = list.Select(x => x.FromUserID).Distinct().ToList();
-            var toarray = list.Select(x => x.ToUserID).Distinct().ToList();
-            fromarray.AddRange(toarray);
-            var allusers = new SubSonic.Query.Select(Repository.GetProvider(), "ID", "NickName", "Avatar").From<User>().And("ID").In(fromarray.ToArray()).ExecuteTypedList<User>();
-            var newlist = (from l in list
-                           select new
-                           {
-                               ID = l.ID,
-                               Avatar = GetFullUrl(allusers.Exists(x => x.ID == l.FromUserID) ? allusers.FirstOrDefault(x => x.ID == l.FromUserID).Avatar : ""),
-                               FromUserName = allusers.Exists(x => x.ID == l.FromUserID) ? allusers.FirstOrDefault(x => x.ID == l.FromUserID).NickName : "",
-                               ToUserName = allusers.Exists(x => x.ID == l.ToUserID) ? allusers.FirstOrDefault(x => x.ID == l.ToUserID).NickName : "",
-                               CreateDate = l.CreateDate.ToString("yyyy-MM-dd"),
-                               Money = l.Money
-                           }).ToList();
-            var result = new
+                var fromarray = list.Select(x => x.FromUserID).Distinct().ToList();
+                var toarray = list.Select(x => x.ToUserID).Distinct().ToList();
+                fromarray.AddRange(toarray);
+                var allusers = new SubSonic.Query.Select(Repository.GetProvider(), "ID", "NickName", "Avatar").From<User>().And("ID").In(fromarray.ToArray()).ExecuteTypedList<User>();
+                var newlist = (from l in list
+                               select new
+                               {
+                                   ID = l.ID,
+                                   FromUserAvatar = GetFullUrl(allusers.Exists(x => x.ID == l.FromUserID) ? allusers.FirstOrDefault(x => x.ID == l.FromUserID).Avatar : ""),
+                                   FromUserName = allusers.Exists(x => x.ID == l.FromUserID) ? allusers.FirstOrDefault(x => x.ID == l.FromUserID).NickName : "",
+                                   ToUserAvatar = GetFullUrl(allusers.Exists(x => x.ID == l.ToUserID) ? allusers.FirstOrDefault(x => x.ID == l.ToUserID).Avatar : ""),
+                                   ToUserName = allusers.Exists(x => x.ID == l.ToUserID) ? allusers.FirstOrDefault(x => x.ID == l.ToUserID).NickName : "",
+                                   CreateDate = l.CreateDate.ToString("yyyy-MM-dd"),
+                                   Money = l.Money
+                               }).ToList();
+                var result = new
+                {
+                    currpage = pager.Index,
+                    records = recordCount,
+                    totalpage = totalPage,
+                    list = newlist
+                };
+                return Content(callback + "(" + Newtonsoft.Json.JsonConvert.SerializeObject(result) + ")");
+            }
+            catch (Exception ex)
             {
-                page = pager.Index,
-                records = recordCount,
-                total = totalPage,
-                rows = newlist
-            };
-            return Json(result, JsonRequestBehavior.AllowGet);
+                LogHelper.ErrorLoger.Error(ex.Message);
+                return Content(callback + "()");
+            }
         }
 
         #endregion
